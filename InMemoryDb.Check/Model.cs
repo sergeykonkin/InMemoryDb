720d614aa55d26e75ddf0145836229e735a6245d
Sergey Konkin <batareeed@gmail.com>
2018-07-04 16:55:47 +0300

    Update Check project model

--- InMemoryDb.Check/Model.cs
+++ InMemoryDb.Check/Model.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InMemoryDb.Check
 {
@@ -18,27 +19,6 @@ namespace InMemoryDb.Check
     {
         public int UserId;
         public int GroupId;
-
-        protected bool Equals(UserGroup other)
-        {
-            return UserId == other.UserId && GroupId == other.GroupId;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((UserGroup) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (UserId * 397) ^ GroupId;
-            }
-        }
     }
 
     public class Group
